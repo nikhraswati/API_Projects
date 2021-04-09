b64afeeb2af1f9088ea99bf0fe2a1e036fedf237
nikhraswati <82213738+nikhraswati@users.noreply.github.com>
2021-04-09 16:45:09 +0530

    Uploaded Web API proj
    Uploaded Web API proj

---
+++ ShopBridge/ShopBridge/BasicAuthentication.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Web;
+using System.Web.Http.Controllers;
+using System.Web.Http.Filters;
+
+namespace ShopBridge
+{
+    public class BasicAuthentication: AuthorizationFilterAttribute
+    {
+        public override void OnAuthorization(HttpActionContext actionContext)
+        {
+            if (actionContext.Request.Headers.Authorization == null)
+            {
+                actionContext.Response = actionContext.Request.CreateResponse(System.Net.HttpStatusCode.Unauthorized);
+            }
+            else
+            {
+                string username;
+                string password;
+                string token = actionContext.Request.Headers.Authorization.Parameter;
+                string DecodedToken = Encoding.UTF8.GetString(Convert.FromBase64String(token));
+                var Credential = DecodedToken.Split(':');
+                username = Credential[0];
+                password = Credential[1];
+                if(username.ToLower()=="thinkbridge" && password=="Thinkbridge@123")
+                {
+                    
+                }
+                else
+                    actionContext.Response = actionContext.Request.CreateResponse(System.Net.HttpStatusCode.Unauthorized);
+
+            }
+        }
+    }
+}
---
+++ ShopBridge/ShopBridge/Controllers/ValuesController.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace ShopBridge.Controllers
+{
+    [BasicAuthentication]
+    public class ValuesController : ApiController
+    {
+        
+        DataAccessLayer dal = new DataAccessLayer();
+        // GET api/values
+
+        [HttpGet]
+        public async Task<HttpResponseMessage> FetchProduct()
+        {
+            try
+            {
+                Task<List<Product>> task = new Task<List<Product>>(dal.GetProducts);
+                task.Start();
+                var result = await task;
+                return Request.CreateResponse(HttpStatusCode.OK, result);
+            }
+            catch(Exception e)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadGateway,"");
+            }
+
+        }
+
+        // GET api/values/5
+        [HttpGet]
+        public async Task<HttpResponseMessage> FetchProductById(int id)
+        {
+            try
+            {
+                var result = await Task.FromResult<Product>(dal.GetProductById(id));
+            
+           
+                return Request.CreateResponse(HttpStatusCode.OK, result);
+            }
+            catch(Exception e)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadGateway, "");
+
+            }
+        }
+
+        // POST api/values
+        [HttpPost]
+        public async Task<HttpResponseMessage> InsertProduct([FromBody]Product prod)
+        {
+            try
+            {
+              var a= await Task.FromResult<bool>(dal.AddProduct(prod));
+                return Request.CreateResponse(HttpStatusCode.OK, "Record inserted successfully");
+
+            }
+            catch (Exception e)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadGateway, "Error occurred while inserting record");
+            }
+        }
+
+        // PUT api/values/5
+        [HttpPut]
+        public async Task<HttpResponseMessage> ModifyProuct(Product p)
+        {
+            try
+            {
+                var a = await Task.FromResult<bool>(dal.UpdateProdList(p));
+
+
+                return Request.CreateResponse(HttpStatusCode.OK, "Record updated successfully");
+
+            }
+            catch (Exception e)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadGateway, "Error occurred while updating record");
+
+            }
+
+        }
+
+        // DELETE api/values/5
+        [HttpDelete]
+        public async Task<HttpResponseMessage> RemoveProduct(int id)
+        {
+            try
+            {
+                var a = await Task.FromResult<bool>(dal.DeleteProduct(id));
+
+
+                return Request.CreateResponse(HttpStatusCode.OK, "Record deleted successfully");
+
+            }
+            catch (Exception e)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadGateway, "Error occurred while deleting the record");
+            }
+        }
+
+        //[HttpGet]
+        //public HttpResponseMessage ReturnAPIResponse(List<Product> message,HttpStatusCode code,)
+        //{
+        //    return Request.CreateResponse(code, message);
+
+        //}
+        
+        //public HttpResponseMessage ReturnAPIErrorResponse(string message, HttpStatusCode code)
+        //{
+        //    return Request.CreateErrorResponse(code, message);
+
+        //}
+    }
+}
